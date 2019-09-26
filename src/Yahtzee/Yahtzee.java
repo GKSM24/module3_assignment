5fb8be4c620b8fcefee3f9a5a5fb4825269a7e42
GKSM24 <kkausshawlsaim24@gmail.com>
2019-09-26 16:34:15 +0530

    yahtzee modified

--- src/Yahtzee/Yahtzee.java
+++ src/Yahtzee/Yahtzee.java
@@ -5,12 +5,22 @@ public class Yahtzee {
         Yahtzee yt = new Yahtzee();
         System.out.println(yt.diceCount());
     }
+
+    /**
+     * This method generates a random number from 1 - 6(including).
+     * @return a random number between 1 - 6
+     */
     public int rollDice(){
         double luckyNumber = Math.random();
         luckyNumber = luckyNumber * 6;
         luckyNumber = luckyNumber + 1;
         return (int) luckyNumber;
     }
+
+    /**
+     * This method counts the number of dice counts required to get a yahtzee.
+     * @return an integer which is the number of dice counts for getting a yahtzee
+     */
     public int diceCount(){
         int dice1 = rollDice(), dice2 = rollDice(), dice3 = rollDice(), dice4 = rollDice(),dice5 = rollDice(), count = 1;
         for(;!(dice1==dice2 && dice2==dice3 && dice3==dice4 && dice4==dice5);){
